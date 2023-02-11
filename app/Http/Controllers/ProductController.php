726b08122fdbdbd45729984aabbb6544071be7e1
programmerkoji <60305092+programmerkoji@users.noreply.github.com>
2023-02-11 21:24:47 +0900

    product_crud

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,8 +3,50 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Repositories\ProductRepository;
 
 class ProductController extends Controller
 {
-    //
+    /**
+     * @var ProductRepository
+     */
+    protected $productRepository;
+
+    /**
+     * @param ProductRepository
+     */
+    public function __construct(ProductRepository $productRepository)
+    {
+        return $this->productRepository =$productRepository;
+    }
+
+    public function index()
+    {
+        return $this->productRepository->index();
+    }
+
+    /**
+     * @param Request $request
+     */
+    public function store(Request $request)
+    {
+        return $this->productRepository->store($request->toArray());
+    }
+
+    /**
+     * @param Request $request
+     * @param int $id
+     */
+    public function update(Request $request, int $id)
+    {
+        return $this->productRepository->update($request->toArray(), $id);
+    }
+
+    /**
+     * @param int $id
+     */
+    public function destroy(int $id)
+    {
+        return $this->productRepository->destroy($id);
+    }
 }
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,8 +4,25 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use App\Models\ProductCategory;
 
 class Product extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'product_category_id',
+        'name',
+        'price',
+        'number',
+    ];
+
+    /**
+     * @return BelongsTo
+     */
+    public function product_category(): BlongsTo
+    {
+        return $this->belongsTo(ProductCategory::class);
+    }
 }
